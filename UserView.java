5d09b05528822eac4aa23688d3dc899a463ad6c1
ixmg3 <madi.nurkenov@gmail.com>
2024-10-27 16:55:35 +0500

    added view for MVC

---
+++ UserView.java
@@ -0,0 +1,6 @@
+// View class to display user information
+public class UserView {
+    public void displayUserInfo(String name, String email) {
+        System.out.println("User: " + name + ", Email: " + email);
+    }
+}
